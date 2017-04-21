9c98ede92d1ca67ad573d4dc616dcf39ba3a428f
mick <figura.m@web.de>
2017-04-21 15:07:25 +0200

    added a Add method to the analizationResult class, so you can add one to another

--- app/src/main/java/com/example/mick/emotionanalizer/AnalizationResult.java
+++ app/src/main/java/com/example/mick/emotionanalizer/AnalizationResult.java
@@ -48,6 +48,42 @@ public class AnalizationResult{
 	public HashMap<String,Integer> wordStatistic_sentiment_negative = new HashMap<String,Integer>();
 	public HashMap<String,Integer> wordStatistic_sentiment_positive = new HashMap<String,Integer>();
 
+
+	/**
+	 * Adds another instance of analization Result to this one
+	 * @param ar
+     */
+	public void Add(AnalizationResult ar){
+		this.weigthing.add(ar.weigthing);
+		this.wordCount += ar.wordCount;
+		this.wordCountAnalized +=ar.wordCountAnalized;
+
+		this.mergeHashTables(this.wordStatistic_all, ar.wordStatistic_all);
+		this.mergeHashTables(this.wordStatistic_anger, ar.wordStatistic_anger);
+		this.mergeHashTables(this.wordStatistic_anticipation, ar.wordStatistic_anticipation);
+		this.mergeHashTables(this.wordStatistic_disgust, ar.wordStatistic_disgust);
+		this.mergeHashTables(this.wordStatistic_fear, ar.wordStatistic_fear);
+		this.mergeHashTables(this.wordStatistic_joy, ar.wordStatistic_joy);
+		this.mergeHashTables(this.wordStatistic_sadness, ar.wordStatistic_sadness);
+		this.mergeHashTables(this.wordStatistic_surprise, ar.wordStatistic_surprise);
+		this.mergeHashTables(this.wordStatistic_trust, ar.wordStatistic_trust);
+		this.mergeHashTables(this.wordStatistic_sentiment_negative, ar.wordStatistic_sentiment_negative);
+		this.mergeHashTables(this.wordStatistic_sentiment_positive, ar.wordStatistic_sentiment_positive);
+
+	}
+
+	private void mergeHashTables(HashMap<String,Integer> a, HashMap<String,Integer> b){
+		Iterator it = b.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry pair = (Map.Entry)it.next();
+			if(a.contains(pair.getKey())){
+				a.put(pair.getKey(),a.get(pair.getKey()) + pair.getValue());
+			}else{
+				a.put(pair.getKey(),pair.getValue());
+			}
+		}
+	}
+
 	/*
 	public AnalizationResult(){
 	//public AnalizationResult(EmotionWeighting weigthing, HashMap<String,Integer> wordStatistic_all, int wordCount, int wordCountAnalized){
